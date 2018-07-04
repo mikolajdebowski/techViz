e2812fe80abb8f6050560c2ccd6b3914449d276a
debowskim <mikolaj.debowski@vizexplorer.com>
2018-07-04 16:55:02 +1200

    Adding initial server config and login views

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,8 @@
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
+import 'package:techviz/components/vizElevated.dart';
 import 'package:techviz/home.dart';
+import 'package:techviz/login.dart';
 
 class Config extends StatefulWidget {
   static final String SERVERURL = 'SERVERURL';
@@ -28,34 +30,85 @@ class ConfigState extends State<Config> {
   void onNextTap() async {
     await prefs.setString(Config.SERVERURL, serverAddressController.text);
 
-    Navigator.push<Home>(
+    Navigator.push<Login>(
       context,
-      MaterialPageRoute(builder: (context) => Home()),
+      MaterialPageRoute(builder: (context) => Login()),
     );
 
   }
 
   @override
   Widget build(BuildContext context) {
+
+    var textFieldStyle = TextStyle(
+        fontStyle: FontStyle.italic,
+        fontSize: 28.0,
+        color: Color(0xFFffffff),
+        fontWeight: FontWeight.w500,
+        fontFamily: "Roboto");
+
+    var textFieldBorder = OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(4.0)));
+    var textFieldPadding = EdgeInsets.only(bottom: 5.0);
+    var textFieldContentPadding = new EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0);
+
+    var backgroundDecoration = BoxDecoration(
+        gradient: LinearGradient(
+            colors: [Color(0xFFd6dfe3), Color(0xFFb1c2cb)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            tileMode: TileMode.repeated));
+
     return Scaffold(
         body: Container(
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [Color(0xFFD2DEE1), Color(0xFFB2C2CB)],
-              begin: Alignment.topCenter,
-              end: Alignment.topCenter,
-              tileMode: TileMode.repeated)),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-              flex: 3,
-              child: TextField(
-                controller: serverAddressController,
-                decoration: InputDecoration(hintText: 'Server Address'),
-              )),
-          Expanded(child: FlatButton(onPressed: onNextTap, child: Text('Next')))
-        ],
-      ),
-    ));
+            decoration: backgroundDecoration,
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(right: 10.0),
+                          child: Container(
+                            width: 400.0,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Padding(
+                                  padding: textFieldPadding,
+                                  child: TextField(
+                                      controller: serverAddressController,
+                                      decoration: InputDecoration(
+                                          fillColor: Colors.black87,
+                                          filled: true,
+                                          hintStyle: textFieldStyle,
+                                          hintText: 'Server Address',
+                                          border: textFieldBorder,
+                                          contentPadding: textFieldContentPadding),
+                                      style: textFieldStyle),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Container(
+                            width: 110.0,
+                            height: 110.0,
+                            child: VizElevated(
+                                onTap: onNextTap,
+                                title: 'Login',
+                                customBackground: [Color(0xFFFFFFFF), Color(0xFFAAAAAA)]))
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )));
   }
 }
--- lib/login.dart
+++ lib/login.dart
@@ -1,15 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:techviz/components/vizElevated.dart';
+import 'package:techviz/home.dart';
 
-class Login extends StatefulWidget{
+class Login extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => LoginState();
 }
 
-class LoginState extends State<Login>{
+class LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
+    var textFieldStyle = TextStyle(
+        fontStyle: FontStyle.italic,
+        fontSize: 28.0,
+        color: Color(0xFFffffff),
+        fontWeight: FontWeight.w500,
+        fontFamily: "Roboto");
+
+    var textFieldBorder = OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(4.0)));
+    var textFieldPadding = EdgeInsets.only(bottom: 5.0);
+    var textFieldContentPadding = new EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0);
+
+    var backgroundDecoration = BoxDecoration(
+        gradient: LinearGradient(
+            colors: [Color(0xFFd6dfe3), Color(0xFFb1c2cb)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            tileMode: TileMode.repeated));
+
+    void onNextTap() async {
+      Navigator.push<Home>(
+        context,
+        MaterialPageRoute(builder: (context) => Home()),
+      );
+
+    }
+
     return Scaffold(
-        body: Center(child: Text('Login', style: TextStyle(fontSize: 30.0)))
-    );
+        body: Container(
+            decoration: backgroundDecoration,
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(right: 10.0),
+                          child: Container(
+                            width: 400.0,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Padding(
+                                  padding: textFieldPadding,
+                                  child: TextField(
+                                      decoration: InputDecoration(
+                                          fillColor: Colors.black87,
+                                          filled: true,
+                                          hintStyle: textFieldStyle,
+                                          hintText: 'Username',
+                                          border: textFieldBorder,
+                                          contentPadding: textFieldContentPadding),
+                                      style: textFieldStyle),
+                                ),
+                                Padding(
+                                  padding: textFieldPadding,
+                                  child: TextField(
+                                      obscureText: true,
+                                      decoration: InputDecoration(
+                                          fillColor: Colors.black87,
+                                          filled: true,
+                                          hintText: 'Password',
+                                          hintStyle: textFieldStyle,
+                                          border: textFieldBorder,
+                                          contentPadding: textFieldContentPadding),
+                                      style: textFieldStyle),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Container(
+                            width: 110.0,
+                            height: 110.0,
+                            child: VizElevated(
+                                onTap: onNextTap,
+                                title: 'Login',
+                                customBackground: [Color(0xFFFFFFFF), Color(0xFFAAAAAA)]))
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )));
   }
+
+  void buttonPressed() {}
 }
