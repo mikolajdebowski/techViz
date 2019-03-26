bf6f0738568cf0940d6d03f442e09b3235a9baeb
mathiasr <rodrigo.mathias@vizexplorer.com>
2019-03-26 17:37:28 +1300

    v07b45

--- ios/release.sh
+++ ios/release.sh
@@ -13,8 +13,7 @@ flutter build ios
 xcodebuild -workspace ios/Runner.xcworkspace \
             -scheme Runner -sdk iphoneos \
             -configuration Release archive \
-            -archivePath ios/Temp/Build/v07b44.xcarchive
+            -archivePath ios/Temp/Build/v07b45.xcarchive
 
-xcodebuild -exportArchive \
-            -archivePath ios/Temp/Build/v07b44.xcarchive \
-            -exportOptionsPlist ios/Runner/exportOptionsAdHoc.plist -exportPath ios/Temp/Build/v07b44
+
+
