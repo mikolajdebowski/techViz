79f53275e5b2e790172a782b70ccd41e5efd9103
mathiasr <rodrigo.mathias@vizexplorer.com>
2019-03-27 13:21:04 +1300

    ACT-1357 - Select Task Type error shown when changing escalation path Every time user changes Escalation Path, the selected task type is being set as null.

---
+++ release.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# change Runner/Info.plist CFBundleVersion and CFBundleShortVersionString
+version = "v07b46"
+
+printf version
+
+flutter clean
+
+flutter analyze
+
+flutter test
+
+flutter build ios
+
+xcodebuild -workspace ios/Runner.xcworkspace \
+            -scheme Runner -sdk iphoneos \
+            -configuration Release archive \
+            -archivePath ios/Temp/Build/v07b46.xcarchive
+
+xcodebuild -exportArchive \
+            -archivePath ios/Temp/Build/v07b46.xcarchive \
+            -exportOptionsPlist ios/Runner/exportOptionsAdHoc.plist -exportPath ios/Temp/Build/v07b46
+
