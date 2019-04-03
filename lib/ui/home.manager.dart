f4c991d42b3bd06c9cd6a55943ab7e11b7e46a11
mathiasr <rodrigo.mathias@vizexplorer.com>
2019-04-03 17:07:37 +1300

    ACT-1374

--- lib/ui/home.manager.dart
+++ lib/ui/home.manager.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
+import 'package:techviz/components/vizSummaryHeader.dart';
 import 'package:techviz/model/userStatus.dart';
+import 'package:techviz/presenter/openTasksPresenter.dart';
 import 'package:techviz/ui/home.dart';
 
 class HomeManager extends StatefulWidget {
@@ -10,9 +12,41 @@ class HomeManager extends StatefulWidget {
   State<StatefulWidget> createState() => HomeManagerState();
 }
 
-class HomeManagerState extends State<HomeManager> implements TechVizHome {
+class HomeManagerState extends State<HomeManager> implements TechVizHome, IOpenTasksPresenter {
+
+  VizSummaryHeader _openTasks;
+  VizSummaryHeader _teamAvailability;
+  VizSummaryHeader _slotFloor;
+
+  OpenTasksPresenter _presenter;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _presenter = OpenTasksPresenter(this);
+    _presenter.loadOpenTasks();
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
+
+    Widget openTasksWidget = _openTasks;
+    if(openTasksWidget==null){
+      openTasksWidget = CircularProgressIndicator();
+    }
+
+    Widget teamAvailabilityWidget = _teamAvailability;
+    if(teamAvailabilityWidget==null){
+      teamAvailabilityWidget = CircularProgressIndicator();
+    }
+
+    Widget slotFloorWidget = _slotFloor;
+    if(slotFloorWidget==null){
+      slotFloorWidget = CircularProgressIndicator();
+    }
 
     BoxDecoration boxDecoration = BoxDecoration(
         borderRadius: BorderRadius.circular(6.0),
@@ -23,11 +57,11 @@ class HomeManagerState extends State<HomeManager> implements TechVizHome {
     Column column = Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-        Expanded(child: (Container(decoration: boxDecoration, child: Center(child: Text('Open tasks'),)))),
+        Expanded(child: (Container(decoration: boxDecoration, child: Center(child: openTasksWidget)))),
         SizedBox(height: 5),
-        Expanded(child: (Container(decoration: boxDecoration, child: Center(child: Text('Team Availability'),)))),
+        Expanded(child: (Container(decoration: boxDecoration, child: Center(child: teamAvailabilityWidget)))),
         SizedBox(height: 5),
-        Expanded(child: (Container(decoration: boxDecoration, child: Center(child: Text('Slot Floor Summary'),)))),
+        Expanded(child: (Container(decoration: boxDecoration, child: Center(child: slotFloorWidget)))),
       ],
     );
 
@@ -49,4 +83,19 @@ class HomeManagerState extends State<HomeManager> implements TechVizHome {
   void onUserStatusChanged(UserStatus us) {
     // TODO: implement onUserStatusChanged
   }
+
+  @override
+  void onLoadError(dynamic error) {
+    // TODO: implement onLoadError
+  }
+
+  @override
+  void onOpenTasksLoaded(VizSummaryHeader summaryHeader) {
+    setState(() {
+      _openTasks = summaryHeader;
+      _teamAvailability = summaryHeader;
+      _slotFloor = summaryHeader;
+    });
+  }
+
 }
