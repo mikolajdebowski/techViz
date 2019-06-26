5af052c59444ceabd477febe8955ffa12399215f
mathiasr <rodrigo.mathias@vizexplorer.com>
2019-06-26 17:38:18 +1200

    ACT-1469 - adding default screen size for testing. This avoids render overflow exception

--- test/ui/taskView_test.dart
+++ test/ui/taskView_test.dart
@@ -8,12 +8,17 @@ import '../repository/async/mock/taskRoutingMock.dart';
 import '../repository/processor/mock/TaskRemoteRepositoryMock.dart';
 
 void main() {
+	final TestWidgetsFlutterBinding binding = TestWidgetsFlutterBinding.ensureInitialized();
+	const Size _kDefaultTestViewportSizeIphone5 = Size(1136,640);
+
 	setUp((){
 		kiwi.Container container = kiwi.Container();
 		container.registerInstance(TaskRepository(TaskRemoteRepositoryMock(), null, TaskRoutingMock()));
 	});
 
 	testWidgets('Pumps TaskView', (WidgetTester tester) async {
+		await binding.setSurfaceSize(_kDefaultTestViewportSizeIphone5);
+
 		await tester.runAsync(() async {
 			await tester.pumpWidget(MaterialApp(home: TaskView(GlobalKey())));
 		});
