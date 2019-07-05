f88dfda58737c2b9b5fd07e0eb6ba752fc648ba0
Rodrigo Mathias <rodrigo.mathias@vizexplorer.com>
2019-07-05 16:32:57 +1200

    Update android gradle parameters for dynamic build version

--- scripts/release_android.sh
+++ scripts/release_android.sh
@@ -24,13 +24,3 @@ curl \
   -F "ipa=@build/app/outputs/apk/release/app-release.apk" \
   -H "X-HockeyAppToken: a14bddac17c24ce1b81a2791fc673272" \
   https://rink.hockeyapp.net/api/2/apps/upload
-
-US:
-ACT-1344 - (Open) Tasks Summary
-ACT-1398 - Make reservation
-
-Tasks:
-ACT-1421 - Count of Overdue open tasks are not colored red
-ACT-1422 - Count of Out of Service machines are not colored red
-ACT-1424 - It is possible to reserve a machine that is IN USE on Active Games tab
-ACT-1411 - [Improvement] "User" column does not make sense on Unassigned metric for Open Tasks because it would be always blank
