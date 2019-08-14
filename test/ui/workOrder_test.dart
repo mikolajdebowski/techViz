e850072b9148147e9cf3fe9b8caa59019a92d5a9
Rodrigo Mathias <rodrigo.mathias@vizexplorer.com>
2019-08-15 09:30:57 +1200

    Removed unused imports

--- test/ui/workOrder_test.dart
+++ test/ui/workOrder_test.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
-import 'package:techviz/components/VizButton.dart';
-import 'package:techviz/components/form/vizDropDownFormField.dart';
 import 'package:techviz/model/taskType.dart';
 import 'package:techviz/model/user.dart';
 import 'package:techviz/repository/repository.dart';
