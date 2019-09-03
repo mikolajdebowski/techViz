ea3ac74dafee2b61e21ad7579f405ad5a6e72942
Rodrigo Mathias <rodrigo.mathias@vizexplorer.com>
2019-09-03 15:27:34 +1200

    ACT-842

---
+++ test/_mocks/mqttInternalClientMock.dart
@@ -0,0 +1,47 @@
+
+import 'dart:async';
+
+import 'package:mqtt_client/mqtt_client.dart' as mqtt;
+import 'package:typed_data/typed_data.dart' as typed;
+
+class MqttInternalClientMock extends mqtt.MqttClient{
+	MqttInternalClientMock(
+			String broker,
+			String deviceID
+			) : super(broker, deviceID);
+	mqtt.MessageIdentifierDispenser messageIdentifierDispenser = mqtt.MessageIdentifierDispenser();
+
+	final StreamController<List<mqtt.MqttReceivedMessage<mqtt.MqttMessage>>> _updatesStream = StreamController<List<mqtt.MqttReceivedMessage<mqtt.MqttMessage>>>();
+
+	@override
+	Future<mqtt.MqttClientConnectionStatus> connect([String username, String password]){
+		var status = mqtt.MqttClientConnectionStatus();
+		status.state = mqtt.MqttConnectionState.connected;
+		updates = _updatesStream.stream;
+		return Future.value(status);
+	}
+
+	@override
+	void disconnect() {
+		_updatesStream?.close();
+	}
+
+	@override
+	mqtt.Subscription subscribe(String topic, mqtt.MqttQos qosLevel) {
+		return mqtt.Subscription();
+	}
+
+	@override
+	int publishMessage(String topic, mqtt.MqttQos qualityOfService, typed.Uint8Buffer data, {bool retain = false}) {
+		final int msgId = messageIdentifierDispenser.getNextMessageIdentifier();
+		mqtt.MqttPublishMessage _message = mqtt.MqttPublishMessage()
+				.toTopic(topic)
+				.withMessageIdentifier(msgId)
+				.withQos(qualityOfService)
+				.publishData(data);
+
+		mqtt.MqttReceivedMessage<mqtt.MqttMessage> messageReceived = mqtt.MqttReceivedMessage<mqtt.MqttMessage>(topic, _message);
+		_updatesStream.add([messageReceived]);
+		return msgId;
+	}
+}
--- test/ui/networkIndicator_test.dart
+++ test/ui/networkIndicator_test.dart
@@ -5,50 +5,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mqtt_client/mqtt_client.dart' as mqtt;
 import 'package:techviz/service/client/MQTTClientService.dart';
 import 'package:techviz/ui/networkIndicator.dart';
-import 'package:typed_data/typed_data.dart' as typed;
 
+import '../_mocks/mqttInternalClientMock.dart';
 
-class MqttInternalClientMock extends mqtt.MqttClient{
-  MqttInternalClientMock() : super('tvdev.internal.bis2.net', '4D8E280D-B840-4773-898D-0F9F71B82ACA');
-  mqtt.MessageIdentifierDispenser messageIdentifierDispenser = mqtt.MessageIdentifierDispenser();
-
-  final StreamController<List<mqtt.MqttReceivedMessage<mqtt.MqttMessage>>> _updatesStream = StreamController<List<mqtt.MqttReceivedMessage<mqtt.MqttMessage>>>();
-
-  @override
-  Future<mqtt.MqttClientConnectionStatus> connect([String username, String password]){
-    var status = mqtt.MqttClientConnectionStatus();
-    status.state = mqtt.MqttConnectionState.connected;
-    updates = _updatesStream.stream;
-    return Future.value(status);
-  }
-
-  @override
-  void disconnect() {
-    _updatesStream?.close();
-  }
-
-  @override
-  mqtt.Subscription subscribe(String topic, mqtt.MqttQos qosLevel) {
-    return mqtt.Subscription();
-  }
-
-  @override
-  int publishMessage(String topic, mqtt.MqttQos qualityOfService, typed.Uint8Buffer data, {bool retain = false}) {
-    final int msgId = messageIdentifierDispenser.getNextMessageIdentifier();
-    mqtt.MqttPublishMessage _message = mqtt.MqttPublishMessage()
-        .toTopic(topic)
-        .withMessageIdentifier(msgId)
-        .withQos(qualityOfService)
-        .publishData(data);
-
-    mqtt.MqttReceivedMessage<mqtt.MqttMessage> messageReceived = mqtt.MqttReceivedMessage<mqtt.MqttMessage>(topic, _message);
-    _updatesStream.add([messageReceived]);
-    return msgId;
-  }
-}
 
 void main(){
 
@@ -60,7 +21,7 @@ void main(){
 
   MqttInternalClientMock _client;
   setUpAll(() async{
-    _client = MqttInternalClientMock();
+    _client = MqttInternalClientMock('tvdev.internal.bis2.net', '4D8E280D-B840-4773-898D-0F9F71B82ACA');
     await MQTTClientService().init('tvdev.internal.bis2.net', '4D8E280D-B840-4773-898D-0F9F71B82ACA', internalMqttClient: _client, logging: false);
     await MQTTClientService().connect();
 
@@ -105,6 +66,7 @@ void main(){
     NetworkIndicator indicator = NetworkIndicator();
     await tester.pumpWidget(makeTestableWidget(child: indicator));
 
+    // ignore: deprecated_member_use
     await BinaryMessages.handlePlatformMessage(
       Connectivity.eventChannel.name,
       Connectivity.eventChannel.codec.encodeSuccessEnvelope('none'),
