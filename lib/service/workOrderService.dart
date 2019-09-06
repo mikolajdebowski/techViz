c3cd96a2354674fcbb2bd643ed9f2be4a1c044e0
debowskim <mikolaj.debowski@vizexplorer.com>
2019-09-06 17:09:37 +1200

    ACT-1561 - Refactor Work Order Changes to use MQTT

--- lib/service/workOrderService.dart
+++ lib/service/workOrderService.dart
@@ -1,40 +1,52 @@
 import 'dart:async';
 import 'dart:convert';
+
 import 'package:intl/intl.dart';
-import 'package:rxdart/rxdart.dart';
 import 'package:techviz/common/deviceUtils.dart';
 import 'package:techviz/common/model/deviceInfo.dart';
+
 import 'client/MQTTClientService.dart';
 
 abstract class IWorkOrderService{
   Future create(String userID, int taskTypeID, {String location, String mNumber, String notes, DateTime dueDate});
-//  void listenAsync();
-//  void cancelListening();
-//  void dispose();
 }
 
 class WorkOrderService implements IWorkOrderService{
   static final WorkOrderService _instance = WorkOrderService._internal();
 
-  factory WorkOrderService({IMQTTClientService mqttClientService}) {
+  factory WorkOrderService({IMQTTClientService mqttClientService, IDeviceUtils deviceUtils}) {
     _instance._mqttClientServiceInstance = mqttClientService ??= MQTTClientService();
+    _instance._deviceUtils = deviceUtils ?? DeviceUtils();
     assert(_instance._mqttClientServiceInstance!=null);
     return _instance;
   }
   WorkOrderService._internal();
 
   IMQTTClientService _mqttClientServiceInstance;
-//  final BehaviorSubject<List<String>> _userSectionsListSubject = BehaviorSubject<List<String>>();
-//  Stream<dynamic> _localStream;
-//  Stream<List<String>> get userSectionsList => _userSectionsListSubject.stream;
+  IDeviceUtils _deviceUtils;
 
   @override
   Future create(String userID, int taskTypeID, {String location, String mNumber, String notes, DateTime dueDate})async{
     assert(userID!=null);
 
-    Completer _completer = Completer<List<String>>();
-    String routingKeyForPublish = 'mobile.workorder';
+    DeviceInfo deviceInfo = _deviceUtils.deviceInfo;
 
+    Completer _completer = Completer<List<String>>();
+    String routingKeyForPublish = 'mobile.workorder.update';
+    String routingKeyForCallback = 'mobile.workorder.update.${deviceInfo.DeviceID}';
+
+    _mqttClientServiceInstance.subscribe(routingKeyForCallback);
+    _mqttClientServiceInstance.streams(routingKeyForCallback).listen((dynamic responseCallback) async{
+      _mqttClientServiceInstance.unsubscribe(routingKeyForCallback);
+
+      dynamic jsonWorkOrderResult = json.decode(responseCallback);
+      int validmachine = jsonWorkOrderResult['validmachine'] as int;
+      if(validmachine==0){
+        _completer.completeError('Invalid Location/Asset Number');
+        return;
+      }
+      _completer.complete();
+    });
 
     Map<String, dynamic> payload = <String, dynamic>{};
     payload['userID'] = userID;
@@ -42,6 +54,7 @@ class WorkOrderService implements IWorkOrderService{
     payload['location'] = location;
     payload['taskTypeID'] = taskTypeID;
     payload['mNum'] = mNumber;
+    payload['deviceID'] = deviceInfo.DeviceID;
 
     payload['notes'] = notes;
     payload['dueDate'] = dueDate!=null? DateFormat("yyyy-MM-dd").format(dueDate) : null;
@@ -50,34 +63,5 @@ class WorkOrderService implements IWorkOrderService{
     return _completer.future.timeout(Duration(seconds: 10));
   }
 
-
-//  @override
-//  void cancelListening() {
-//    String deviceID = _deviceUtils.deviceInfo.DeviceID;
-//    _mqttClientServiceInstance.unsubscribe('mobile.sectionlist.$deviceID');
-//  }
-//
-//  @override
-//  void dispose() {
-//    _userSectionsListSubject?.close();
-//  }
-//
-//  @override
-//  void listenAsync() {
-//    String deviceID = _deviceUtils.deviceInfo.DeviceID;
-//    _localStream = _mqttClientServiceInstance.subscribe('mobile.sectionlist.$deviceID');
-//    _localStream.listen((dynamic data){
-//      dynamic json = JsonDecoder().convert(data);
-//
-//      if(json['SectionList'] == null)
-//        return _userSectionsListSubject.add([]);
-//
-//      List<String> sectionList = json['SectionList'].toString().split(',');
-//      if(sectionList.isEmpty)
-//        return _userSectionsListSubject.add([]);
-//
-//      _userSectionsListSubject.add(sectionList.map((String s)=>s.trim()).toList());
-//    });
-//  }
 }
 
